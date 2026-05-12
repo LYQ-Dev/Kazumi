0a88d06ba909399d30d19bf827687df33cfb2133
程翊雪 <1355784761@qq.com>
2026-05-12 12:49:45 +0800

    尝试进行版本统一

--- lib/utils/mortis.dart
+++ lib/utils/mortis.dart
@@ -1,6 +1,6 @@
 // The file contains some interesting imformation about dandan public api.
 // Unusual name to avoid github global search, it's just test code and will be replaced in github actions.
 const Map<String, String> mortis = {
-  'id': 'kvpx7qkqjh',
-  'value': 'rABUaBLqdz7aCSi3fe88ZDj2gwga9Vax',
+  'id': 'YOUR_APPID_HERE',          // 替换为真实的 DANDANAPI_APPID
+  'value': 'YOUR_API_KEY_HERE',     // 替换为真实的 DANDANAPI_KEY
 };
